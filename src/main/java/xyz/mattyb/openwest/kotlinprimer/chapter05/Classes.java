cc80205848ff1a03febc7de93e6506a4532ed90f
Matt Berteaux <mattyb678@gmail.com>
2018-06-08 11:24:54 -0600

    classes

--- src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter05/Classes.java
+++ src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter05/Classes.java
@@ -1,4 +1,19 @@
 package xyz.mattyb.openwest.kotlinprimer.chapter05;
 
 public class Classes {
+    public static void main(String[] args) {
+        Object toCheck = "Checking";
+        boolean isObject = toCheck instanceof Object;
+        boolean isNumber = toCheck instanceof Number;
+        System.out.println("String is object: " + isObject);
+        System.out.println("String is number: " + isNumber);
+
+        if (toCheck instanceof String) {
+            String upper = ((String) toCheck).toUpperCase();
+            System.out.println(upper);
+        }
+
+        String indented = ((String) toCheck).replaceFirst("C", "\tC");
+        System.out.println(indented);
+    }
 }
--- src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter05/classes.kt
+++ src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter05/classes.kt
@@ -1,5 +1,56 @@
 package xyz.mattyb.openwest.kotlinprimer.chapter05
 
 fun main(args: Array<String>) {
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * Don't want to give away how functions work ;-)
+     */
+    val toCheck: Any = "Checking"
 
+    val isAny = toCheck is Any
+    val isNumber = toCheck is Number
+
+    println("String is any: $isAny, is number: $isNumber")
+
+    if (toCheck is String) {
+        val upper = toCheck.toUpperCase()
+
+        println(upper)
+    }
+
+    val indented = (toCheck as String).prependIndent()
+    println(indented)
 }
