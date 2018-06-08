a2dff8cd528a1fee457aec1d91d650e8cbf770c2
Matt Berteaux <mattyb678@gmail.com>
2018-06-08 11:35:54 -0600

    arrays

---
+++ src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter06/arrays.kt
@@ -0,0 +1,48 @@
+package xyz.mattyb.openwest.kotlinprimer.chapter06
+
+fun main(args: Array<String>) {
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * Don't want to give away how functions work ;-)
+     */
+    val arr = arrayOf("Hello", "World")
+
+    println(arr.joinToString(" "))
+
+    var intArr = intArrayOf(1, 2, 3, 4)
+
+    println(intArr.sum())
+}
