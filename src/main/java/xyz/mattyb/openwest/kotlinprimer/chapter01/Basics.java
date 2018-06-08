a87641d7225d347702149e2cfc59a3636dbe8157
Matt Berteaux <mattyb678@gmail.com>
2018-06-08 07:50:33 -0600

    Initial commit

---
+++ src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter01/Basics.java
@@ -0,0 +1,7 @@
+package xyz.mattyb.openwest.kotlinprimer.chapter01;
+
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/main/java/xyz/mattyb/openwest/kotlinprimer/chapter01/Variables.java
@@ -0,0 +1,7 @@
+package xyz.mattyb.openwest.kotlinprimer.chapter01;
+
+public class Variables {
+    public static void main(String[] args) {
+
+    }
+}
